fc9e13b604745ae49e7bdf7952cf0ad41f58f131
Achiket Kumar <126181868+Achiket123@users.noreply.github.com>
2025-08-25 23:45:36 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,23 +11,4 @@ func main() {
 	}
 }
 
-/*
-curl -X POST "https://generativelanguage.googleapis.com/v1beta/cachedContents?key=AIzaSyBjMl6WI3bClsTlqYRorT5PAj1120dUaQ0" \
--H 'Content-Type: application/json' \
--d @request.json \
-> cache.json
-
-curl -X POST "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-001:generateContent?key=AIzaSyBjMl6WI3bClsTlqYRorT5PAj1120dUaQ0" \
--H 'Content-Type: application/json' \
--d '{
-      "contents": [
-        {
-          "parts":[{
-            "text": "Please summarize this transcript"
-          }],
-          "role": "user"
-        },
-      ],
-      "cachedContent": "'$CACHE_NAME'"
-    }'
-*/
+/
